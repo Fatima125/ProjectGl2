dd456ea19350da8ba299cb8ed456ae69d7a241a4
Fatima125 <fatimaabci380@gmail.com>
2021-01-22 17:48:50 +0100

    My first commit

---
+++ src/main/java/com/Covid19/ProjectGl2/ProjectGl2Application.java
@@ -0,0 +1,13 @@
+package com.Covid19.ProjectGl2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGl2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Covid19/ProjectGl2/ProjectGl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Covid19.ProjectGl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
